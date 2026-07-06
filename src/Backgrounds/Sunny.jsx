f9fe39607166ccff8896d5e9a4c505c65e9ac101
K Lakshmi Narayana <klakshminarayana@Ks-MacBook-Air.local>
2026-07-06 21:43:39 +0530

    klnydb6

--- src/Backgrounds/Sunny.jsx
+++ src/Backgrounds/Sunny.jsx
@@ -3,12 +3,20 @@ import "./Sunny.css";
 export default function Sunny() {
   return (
     <div className="sunny-bg">
+
+      <div className="sun-glow"></div>
+
       <div className="sun"></div>
 
-      <div className="ray ray1"></div>
-      <div className="ray ray2"></div>
-      <div className="ray ray3"></div>
-      <div className="ray ray4"></div>
+      <div className="cloud cloud1"></div>
+      <div className="cloud cloud2"></div>
+      <div className="cloud cloud3"></div>
+
+      <div className="particle p1"></div>
+      <div className="particle p2"></div>
+      <div className="particle p3"></div>
+      <div className="particle p4"></div>
+
     </div>
   );
 }
